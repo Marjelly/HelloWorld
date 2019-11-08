7e074783c77af437388a2b63c8f17c374e4eb16e
Eitel.Martin <Eitel.Martin@DESKTOP-RR8JUNQ.2631.skool.net>
2019-11-08 09:47:18 +0100

    Initialcommit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,23 @@
 
 public class HelloWorld {
+	static int number =0;
 
 	public static void main(String[] args) {
-		
-		System.out.println("HelloWorld");
+	 
+		 for(int a=1;a<1000000000 ;a++){
+			 boolean isPrime=true;
 
+			 number++;
+		for(int i=2;i<= Math.sqrt(number)&&isPrime==true;i++){
+			if(number%i==0){isPrime=false;
+			i=number;
+			
+			}
+			
+		}
+if(isPrime==true){System.out.println(number+" ");}
+ 
+		 }
 	}
 
 }
